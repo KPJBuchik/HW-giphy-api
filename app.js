49dd8f348ce7a680f01a4e5cce4d69df27978235
Kevin Buchik <kbuchik@gmail.com>
2019-07-28 15:00:22 -0700

    ajax call

--- app.js
+++ app.js
@@ -1,99 +1,73 @@
 
 
 
+var gifCategories = ["cat", " incredulous Jaguar Fan", "gil", "360 no scope", "table magic trick"]
 
-var gifCategories = ["Stupid Sexy Flaners", " incredulous Jaguar Fan", "gil", "360 no scope", , "table magic trick"]
 
 //displays gifs 
 function displayGifs() {
+    $(".gif-viewholder").empty();
 
     var gifCategories = $(this).attr("data-name")
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        gifCategories + "&api_key=8rPpM28WAsVUHRzd7O91Q9vpYLSMlFQe";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifCategories + "&api_key=8rPpM28WAsVUHRzd7O91Q9vpYLSMlFQe";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
 
+        for (let i = 0; i < response.data.length; i++) {
+            var gifDiv = $("<div>")
+            var rating = response.data[i].rating;
 
 
 
+            var ratingDisplay = $("<p>").text(rating);
+            var animatedGif = response.data[i].images.fixed_height.url;
+            var stillGif = response.data[i].images.fixed_height_still.url;
 
-        
-        var gifDiv =$("<div class=gif-div-here>")
-        var rating = response.Rated;
-        var ratingDisplay = $("<p>").text("Rating: " + rating);
-        gifDiv.append(ratingDisplay)
-        var imageUrl = response.data.image_original_url;
+            //makes still possible
+            var gifDisplay = $("<img class='allgifs'>")
+            gifDisplay.attr("src", stillGif);
+            gifDisplay.attr("data-still", stillGif)
+            gifDisplay.attr("data-animate", animatedGif)
+            gifDisplay.attr("data-state", "still")
 
-        var gifDisplay = $("<img>").attr("src",imageUrl)
 
-        gifDisplay.attr("src", imageUrl)
-        gifDiv.append(gifDisplay)
-        $("#gif-viewholder").prepend(gifDiv)
+        }
 
-    })
+    });
+};
+
+
+
+function display() {
+
+
+
 
 }
-    function createButtons() {
 
 
-        for (let i = 0; i < gifCategories.length; i++) {
-
-            var gifButtons = $("<button>")
-            gifButtons.addClass("gif-button")
-            gifButtons.attr("data-name", gifCategories[i])
-            gifButtons.text(movies[i])
-            $("#button-view").append(gifButtons);
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
+function 
 
 
 
 /*
-
 $("#buttons").on("click", function () {
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     gif + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-
 $.ajax({
     url: queryURL,
     method: "GET"})
-
     var gif = $(this).attr("data-name")
-
-
     function displayGifs() {
-
-
         $("gifview-holder").img(response)
             .then(function (response) {
                 var imageUrl = response.data.image_original_url;
-
                 var gifDisplay = $("<img>");
                 gifDisplay.attr("src", imageUrl)
-
-
             $("#images").prepend(catImage)
-
-
-
-
-
             }
-
-
         }*/
