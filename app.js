44f65cbe737e891548a76e02a1698c32e44b55f9
Kevin Buchik <kbuchik@gmail.com>
2019-07-29 19:44:31 -0700

    functional

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 
 
-
+$(document).ready();
 var gifCategories = ["cat", " incredulous Jaguar Fan", "gil", "360 no scope", "table magic trick"]
 
 
-//displays gifs 
+function displayButtons() {
+    $(".button-view").empty();
+
+    for (let i = 0; i < gifCategories.length; i++) {
+
+        let newButton = $("<button>")
+        newButton.addClass("categories");
+        newButton.attr("data-name", gifCategories[i]);
+        newButton.text(gifCategories[i])
+        $(".button-view").append(newButton);
+    }
+};
+//displays gifs, ajax call to gather data, for loop to display multiple gifs
 function displayGifs() {
     $(".gif-viewholder").empty();
 
@@ -16,58 +28,74 @@ function displayGifs() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        console.log(response);
 
-        for (let i = 0; i < response.data.length; i++) {
+        for (let i = 0; i < 24; i++) {
             var gifDiv = $("<div>")
             var rating = response.data[i].rating;
-
-
 
             var ratingDisplay = $("<p>").text(rating);
             var animatedGif = response.data[i].images.fixed_height.url;
             var stillGif = response.data[i].images.fixed_height_still.url;
 
             //makes still possible
-            var gifDisplay = $("<img class='allgifs'>")
+            var gifDisplay = $("<img>")
+            gifDisplay.addClass("allgifs")
             gifDisplay.attr("src", stillGif);
             gifDisplay.attr("data-still", stillGif)
             gifDisplay.attr("data-animate", animatedGif)
             gifDisplay.attr("data-state", "still")
 
 
+            //displays ratings after images
+            gifDiv.append(ratingDisplay)
+            gifDiv.prepend(gifDisplay)
+            $(".gif-viewholder").append(gifDiv)
+
+
         }
 
     });
-};
+}
+
+function searchGifs (){
 
 
-
-function display() {
 
 
 
 
 }
 
+//adds buttons through input field 
+$("#add-gif").on("click", function (event) {
+    event.preventDefault();
 
-function 
+    var giphy = $("#gif-input").val().trim();
+    displayGifs(giphy)
+
+    gifCategories.push(giphy)
+    displayButtons()
+});
 
 
+$(document).on("click", ".categories", displayGifs);
+displayButtons();
 
-/*
-$("#buttons").on("click", function () {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    gif + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-$.ajax({
-    url: queryURL,
-    method: "GET"})
-    var gif = $(this).attr("data-name")
-    function displayGifs() {
-        $("gifview-holder").img(response)
-            .then(function (response) {
-                var imageUrl = response.data.image_original_url;
-                var gifDisplay = $("<img>");
-                gifDisplay.attr("src", imageUrl)
-            $("#images").prepend(catImage)
-            }
-        }*/
+//pauses gifs 
+$(".gif-viewholder").on("click", ".allgifs", function () {
+    var dataState = $(this).attr("data-state");
+
+    if (dataState === "still") {
+        $(this).attr('src', $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
+    }
+    
+    else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+
+
+    }
+
+})
